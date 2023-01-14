f2213896dff4ff990c3f712bbcfee016d0debc65
Ivan Diaz <ivandiaz94ing@gmail.com>
2023-01-14 13:10:28 -0500

    cargado datos del json equipo

--- src/app/services/infopagina.service.ts
+++ src/app/services/infopagina.service.ts
@@ -1,27 +1,44 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { InfoPagina } from '../interfaces/info-pagina.interface';
+import { InfoEquipo } from '../interfaces/equipo-pagina.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class InfopaginaService {
-
   info: InfoPagina = {};
+  equipo: InfoEquipo = {};
   cargada = false;
 
-  constructor(private http: HttpClient ) {
-   // console.log('Servicio de la infopagina listo');
+  constructor(private http: HttpClient) {
+    // console.log('Servicio de la infopagina listo');
+    this.cargarinfo();
+    this.cargarequipo();
+}
 
+
+  private cargarinfo(){
+
+    
     //Leer el archivo JSON
-    this.http.get('assets/data/data-pagina.json')
-        .subscribe( (resp: InfoPagina) => {
+    this.http
+      .get('assets/data/data-pagina.json')
+      .subscribe((resp: InfoPagina) => {
+        this.cargada = true;
+        this.info = resp;
+        console.log(resp);
+      });
+  
 
-          this.cargada = true;
-          this.info = resp;
-          console.log(resp);   
-        
-        })
+  }
 
-   }
+  private cargarequipo(){
+    this.http.get('assets/data/data-equipo.json')
+    .subscribe((res: InfoEquipo) => {
+      this.equipo = res;
+      console.log(res);
+    
+    });
+  }
 }
